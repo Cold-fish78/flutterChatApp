f2e6293c9f1c74876b50c0816c4d9cdd1fc24e2e
Cold-fish78 <pubg69086@gmail.com>
2022-03-14 14:46:24 +0530

    refactored the code by extractine widget

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat/screens/login_screen.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter_chat/screens/registration_screen.dart';
+import 'package:flutter_chat/components/rounded_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
@@ -9,34 +10,36 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
   late AnimationController animationController;
   late Animation animation;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    animationController = AnimationController(duration: Duration(seconds: 1) , vsync: this,);
+    animationController = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
     // animation = CurvedAnimation(parent: animationController,curve: Curves.decelerate);
-animation = ColorTween(begin: Colors.blueGrey,end: Colors.white).animate(animationController);
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(animationController);
     animationController.forward();
 
     animationController.addListener(() {
-      setState(() {
-
-      });
-
+      setState(() {});
     });
-
   }
+
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
     animationController.dispose();
   }
-  @override
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: animation.value,
@@ -56,7 +59,7 @@ animation = ColorTween(begin: Colors.blueGrey,end: Colors.white).animate(animati
                   ),
                 ),
                 TypewriterAnimatedTextKit(
-                 text : ['Flash Chat'],
+                  text: ['Flash Chat'],
                   textStyle: TextStyle(
                     color: Colors.black,
                     fontSize: 45.0,
@@ -71,43 +74,13 @@ animation = ColorTween(begin: Colors.blueGrey,end: Colors.white).animate(animati
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                    //Go to rNaiegistration screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+            RoundedWidget(color: Colors.lightBlueAccent,title: 'Log In',onPressed: () {
+              Navigator.pushNamed(context, LoginScreen.id);
+            },),
+            RoundedWidget(color: Colors.blueAccent, onPressed: () {
+              Navigator.pushNamed(context, RegistrationScreen.id);
+              //Go to rNaiegistration screen.
+            }, title: 'Register'),
           ],
         ),
       ),
