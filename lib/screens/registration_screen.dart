b6ecbea0d5e2bf994c9c2e4b1164a2653c34e84b
Cold-fish78 <pubg69086@gmail.com>
2022-03-15 15:58:32 +0530

    updated modal progress hud with null_safety version

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat/components/rounded_button.dart';
 import 'package:flutter_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import 'chat_screen.dart';
 class RegistrationScreen extends StatefulWidget {
